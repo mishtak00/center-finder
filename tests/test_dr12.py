766859ead18ea7d6fc41c5899f23a69d0228b510
yujie-liu <yliu134@u.rochester.edu>
2019-04-02 10:55:26 -0400

    test on dr12 south

---
+++ tests/test_dr12.py
@@ -0,0 +1,29 @@
+import numpy as np
+from astropy.io import fits
+import matplotlib.pyplot as plt
+from centerfinder import util
+from centerfinder import blob
+from centerfinder import sky
+
+
+def read_fits(filename):
+    if filename.split('.')[-1] == 'fits':
+        hdul = fits.open(filename)
+        ra = hdul[1].data['RA']
+        dec = hdul[1].data['DEC']
+        z = hdul[1].data['Z']
+        typ_ = hdul[1].data['WEIGHT_FKP'] # placeholder
+        return np.vstack([ra, dec, z, typ_])
+
+
+def test_vote(filename):
+    sky_ = sky.Sky(read_fits(filename), 5)
+    # sky_.find_center(radius=108, blob_size=3, type_='diff')
+    sky_.vote(radius=108)
+    path = filename.split('.')[-2].split('/')[-1]
+    path = '../models/' + path + '_108'
+    print(path, filename)
+    util.pickle_sky(sky_, path)
+
+
+test_vote('../data/galaxy_DR12v5_CMASS_South.fits')
